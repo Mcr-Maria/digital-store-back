ffd5452ed58e0341f9bd432a86b49d5193ee51fa
Mcr-Maria <ceissinha@gmail.com>
2025-06-03 16:38:41 -0300

    token

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,28 @@
 
+const jwt = require("jsonwebtoken");
+
 
 function rotaPrtegida(req, res, next){
-    if(!req.headers['authorization']){
-        res.send({
+    const token = req.headers.authorization;
+
+    if(!token){
+        res.status(401).send({
             tipo: "warning",
             mensagem: "Não autorizado"
         });
 
     }
+    
+    jwt.verify(token.split(" ")[1], process.env.SEGREDO, (error) => {
+        if(error){
+            res.status(401).send({
+                tipo: "warning",
+                messagem: "token inválido"
+            })
+        }
+
+    next();
+})
 }
 
 module.exports = {
